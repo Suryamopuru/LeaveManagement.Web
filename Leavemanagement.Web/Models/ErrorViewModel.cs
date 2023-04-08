6eb6e9cbc7319d04b43b8aa2e6757a2ccd7fb0a9
Surya <suryateja@fluidtouch.biz>
2023-04-08 18:43:15 +0530

    Add project files.

---
+++ Leavemanagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Leavemanagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
